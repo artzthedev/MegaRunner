c843a643552f0fe6b45e5d1a237d01f374dc42ac
Artur Dutra <arturmateusdutrabck@gmail.com>
2025-03-04 11:42:31 -0300

    code reduce to jump animation

--- game/scripts.js
+++ game/scripts.js
@@ -11,3 +11,37 @@ function jump() {
 document.addEventListener("keydown", function (event) {
     jump();
 });
+
+const jumpSpritesAnimation = document.getElementById("player");
+
+const sprites = [
+    "jump1.png", "jump2.png", "jump3.png", "jump4.png",
+    "jump5.png", "jump6.png", "jump7.png", "jump8.png",
+    "jump9.png", "jump10.png", "jump11.png", "jump12.png"
+];
+
+let frame = 0;
+function animateSprite() {
+    player.style.backgroundImage = `url(/chars/megaman_sprites/${sprites[frame]})`;
+    frame = (frame + 1) % sprites.length;
+}
+
+function jump() {
+    if (!player.classList.contains("jump")) {
+        player.classList.add("jump");
+        let spriteInterval = setInterval(animateSprite, 50);
+
+        setTimeout(() => {
+            player.classList.remove("jump");
+            clearInterval(spriteInterval);
+            player.style.backgroundImage = "url(/chars/megaman_sprites/run1.png)"; // Volta para a posição inicial
+        }, 500);
+    }
+}
+
+// Faz o personagem pular quando pressiona a tecla espaço
+document.addEventListener("keydown", (event) => {
+    if (event.code === "Space") {
+        jump();
+    }
+});
