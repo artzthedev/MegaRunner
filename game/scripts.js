352507797cd8798013217930b43346b95ac21239
Artur Dutra <arturmateusdutrabck@gmail.com>
2025-03-07 16:20:38 -0300

    mett sprites and css animation

--- game/scripts.js
+++ game/scripts.js
@@ -1,12 +1,5 @@
 const player = document.getElementById("player");
-
-function jump() {
-    player.classList.add("jump");
-
-    setTimeout(function () {
-        player.classList.remove("jump");
-    }, 500)
-}
+const enemies = document.getElementsByClassName("enemies");
 
 document.addEventListener("keydown", (event) => {
     if (event.code === "Space") {
@@ -18,26 +11,31 @@ document.addEventListener("keydown", (event) => {
 const jumpSpritesAnimation = document.getElementById("player");
 
 const sprites = [
-    "jump1.png", "jump2.png", "jump3.png", "jump4.png",
-    "jump5.png", "jump6.png", "jump7.png", "jump8.png",
-    "jump9.png", "jump10.png", "jump11.png", "jump12.png"
+    "jump3.png", "jump6.png", "jump8.png", "jump8.png",
+    "jump8.png", "jump8.png", "jump10.png", "jump12.png"
 ];
 
 let frame = 0;
 function animateSprite() {
-    player.style.backgroundImage = `url(/chars/megaman_sprites/${sprites[frame]})`;
+    player.style.backgroundImage = `url(/chars/megaman_sprites/Megaman8/${sprites[frame]})`;
     frame = (frame + 1) % sprites.length;
-}
+};
 
 function jump() {
     if (!player.classList.contains("jump")) {
         player.classList.add("jump");
-        let spriteInterval = setInterval(animateSprite, 50);
+        let spriteInterval = setInterval(animateSprite, 75);
 
         setTimeout(() => {
             player.classList.remove("jump");
             clearInterval(spriteInterval);
-            player.style.backgroundImage = "url(/chars/megaman_sprites/run1.png)";
+            player.style.backgroundImage = "url(/chars/megaman_sprites/Megaman8/run1.png)";
         }, 600);
     }
-}
+};
+
+let running = setInterval(function () {
+    let playerTop = parseInt(window.getComputedStyle(player).getPropertyValue("top"));
+
+    let enemiesLeft = parseInt(window.getComputedStyle(enemies).getPropertyValue("left"));
+}, 10);
